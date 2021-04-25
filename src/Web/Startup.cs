19cdd1a1fec074cf59c0a97453691dac095373f5
mhashimm <mhashim.gm@gmail.com>
2021-04-25 10:36:17 -0700

    Reverting back to orginal

--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -54,7 +54,7 @@ namespace LostAndFound.Web
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                //app.UseHsts();
+                app.UseHsts();
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
